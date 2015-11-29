a889ec6e84fe1670c29f40b2aa213de5c5e12c1e
George Vanburgh <george.vanburgh@student.manchester.ac.uk>
2015-11-29 23:14:49 +0000

    Metrics only logged if -statsd flag populated
    Metrics will now only be sent to statsd, if the address of a statsd broker
    has been provided using the -statsd flag. If this flag is not set -
    metrics will still be written to the log, but no attempt will be made to
    deliver them to a statsd broker. This closes #7.

--- metricsmanager.go
+++ metricsmanager.go
@@ -9,15 +9,22 @@ import (
 type MetricsManager struct {
 	metricsChannel chan *Metric
 	statsBuffer    *statsd.StatsdBuffer
+	statsdEnabled  bool
 }
 
 func (m *MetricsManager) Initialize() chan<- *Metric {
 	m.metricsChannel = make(chan *Metric, 100)
 
-	statsClient := statsd.NewStatsdClient("127.0.0.1:8125", "gamq.")
-	statsClient.CreateSocket()
-	interval := time.Second
-	m.statsBuffer = statsd.NewStatsdBuffer(interval, statsClient)
+	if Configuration.StatsDEndpoint != "" {
+		m.statsdEnabled = true
+		statsClient := statsd.NewStatsdClient(Configuration.StatsDEndpoint, "gamq.")
+		statsClient.CreateSocket()
+		interval := time.Second
+		m.statsBuffer = statsd.NewStatsdBuffer(interval, statsClient)
+		log.Debug("Initialized StatsD")
+	} else {
+		m.statsdEnabled = false
+	}
 
 	go m.listenForMetrics()
 
@@ -31,11 +38,14 @@ func (m *MetricsManager) listenForMetrics() {
 		metric = <-m.metricsChannel
 		log.Debugf("Received metric: %s - %d", metric.Name, metric.Value)
 
-		switch metric.Type {
-		case "counter":
-			m.statsBuffer.Incr(metric.Name, metric.Value)
-		case "guage":
-			m.statsBuffer.Gauge(metric.Name, metric.Value)
+		if m.statsdEnabled {
+			log.Debugf("Logging metrics")
+			switch metric.Type {
+			case "counter":
+				m.statsBuffer.Incr(metric.Name, metric.Value)
+			case "guage":
+				m.statsBuffer.Gauge(metric.Name, metric.Value)
+			}
 		}
 	}
 }
