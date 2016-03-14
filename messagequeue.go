21c2b1a8a83cc72fd3ab07c686dd133e99168347
George Vanburgh <george.vanburgh@student.manchester.ac.uk>
2016-03-14 00:46:25 +0000

    Fixed incompatibility with go 1.3

--- messagequeue.go
+++ messagequeue.go
@@ -84,7 +84,7 @@ func (q *messageQueue) closeSubscriber(givenSubscriberName string) {
 }
 
 func (q *messageQueue) logMetrics() {
-	for range time.Tick(time.Second) {
+	for _ = range time.Tick(time.Second) {
 		// Die with the handler
 		if !q.running {
 			break
